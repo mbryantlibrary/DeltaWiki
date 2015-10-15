e409e4a6ca32eb3f0acd3b5c2c81ea3b2e60b60d
Miles Bryant <milesbryant0@gmail.com>
2015-10-15 23:09:36 +0100

    Fixed whitespace
    http://localhost:3456/issues/search#issues=2ba242f3-9c08-4d0c-98d4-4eb3d9ab4de0

--- src/main/java/mb/DeltaWiki/App.java
+++ src/main/java/mb/DeltaWiki/App.java
@@ -9,14 +9,13 @@ import org.glassfish.jersey.jetty.JettyHttpContainerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class App {
-	
-	public static void main(String[] args) throws Exception
-    {
-		
+
+	public static void main(String[] args) throws Exception {
+
 		URI baseUri = UriBuilder.fromUri("http://localhost/").port(8888).build();
 		ResourceConfig config = new ResourceConfig(PageServlet.class);
-		
+
 		Server server = JettyHttpContainerFactory.createServer(baseUri, config);
-		
-    }
+
+	}
 }
